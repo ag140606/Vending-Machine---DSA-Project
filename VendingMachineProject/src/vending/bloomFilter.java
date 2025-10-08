924d054d14a44eb41cc2ebf60596d679a17981d6
Vaishiyy <vaishnavi30.viju@gmail.com>
2025-10-09 01:18:27 +0530

    Update bloomFilter.java

--- VendingMachineProject/src/vending/bloomFilter.java
+++ VendingMachineProject/src/vending/bloomFilter.java
@@ -43,26 +43,30 @@ final class bloomFilter {
 
 // Simple MurmurHash3 flavor (32-bit)
 private static int murmurHash3(String s) {
-        byte[] data = s.getBytes();
+        byte[] data = s.getBytes(); // converts s into an array of bytes
         int length = data.length;
         int h1 = 0;
-        final int c1 = 0xcc9e2d51;
+        final int c1 = 0xcc9e2d51; // MurmurHash magic constant
         final int c2 = 0x1b873593;
         int roundedEnd = (length & 0xfffffffc);  // round down to 4 byte block
 
-        for (int i = 0; i < roundedEnd; i += 4) {
+        for (int i = 0; i < roundedEnd; i += 4) { // processes chunks of 4 bytes
             int k1 = ((data[i] & 0xff)) |
                     ((data[i + 1] & 0xff) << 8) |
                     ((data[i + 2] & 0xff) << 16) |
                     ((data[i + 3] & 0xff) << 24);
+            // mix the bits of k1
             k1 *= c1;
             k1 = Integer.rotateLeft(k1, 15);
             k1 *= c2;
+
+            //mix k1 into main hash
             h1 ^= k1;
             h1 = Integer.rotateLeft(h1, 13);
             h1 = h1 * 5 + 0xe6546b64;
         }
 
+        // handles left-over bytes (less than 4)
         int k1 = 0;
         int tail = length & 0x03;
         if (tail == 3) k1 = (data[roundedEnd + 2] & 0xff) << 16;
@@ -70,16 +74,18 @@ private static int murmurHash3(String s) {
         if (tail >= 1) {
             k1 |= (data[roundedEnd] & 0xff);
             k1 *= c1;
-            k1 = Integer.rotateLeft(k1, 15);
+            k1 = Integer.rotateLeft(k1, 15); // mix leftover bits
             k1 *= c2;
             h1 ^= k1;
         }
 
+        // final mixing
         h1 ^= length;
-        h1 = fmix(h1);
+        h1 = fmix(h1); // final avalanche of bits
         return h1;
     }
 
+    // final avalanche mixing to make has bits uniform
     private static int fmix(int h) {
         h ^= (h >>> 16);
         h *= 0x85ebca6b;
